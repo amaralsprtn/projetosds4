fdf2aedf5f925b66cd993291f7349113c0b603b0
Antonio Amaral <antonio.amaral@monitoratec.com.br>
2021-09-08 20:04:07 -0300

    Project created

---
+++ backend/src/main/java/devsuperior/demo/Sds4Application.java
@@ -0,0 +1,13 @@
+package devsuperior.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sds4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sds4Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/devsuperior/demo/Sds4ApplicationTests.java
@@ -0,0 +1,13 @@
+package devsuperior.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sds4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
