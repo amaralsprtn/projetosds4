7a7d83cf87b3f50fc93201edb5b665f3e80ce52e
Antonio Amaral <antonio.amaral@monitoratec.com.br>
2021-09-08 21:10:44 -0300

    Static charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -14,45 +14,45 @@ const DataTable = () => {
             <tbody>
                 <tr>
                     <td>22/04/2021</td>
-                    <td>Peter Parker</td>
-                    <td>34</td>
-                    <td>25</td>
+                    <td>Dwight Schrute</td>
+                    <td>15</td>
+                    <td>22</td>
+                    <td>15016.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Phyllys Vance</td>
+                    <td>3</td>
+                    <td>23</td>
                     <td>15017.00</td>
                 </tr>
                 <tr>
                     <td>22/04/2021</td>
-                    <td>Bruce Banner</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>Stanley</td>
+                    <td>323</td>
+                    <td>24</td>
+                    <td>15018.00</td>
                 </tr>
                 <tr>
                     <td>22/04/2021</td>
-                    <td>Tony Stark</td>
-                    <td>34</td>
+                    <td>Jim Halpert</td>
+                    <td>1</td>
                     <td>25</td>
-                    <td>15017.00</td>
+                    <td>15019.00</td>
                 </tr>
                 <tr>
                     <td>22/04/2021</td>
-                    <td>Natasha Romanoff</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>Ryan</td>
+                    <td>57</td>
+                    <td>27</td>
+                    <td>15020.00</td>
                 </tr>
                 <tr>
                     <td>22/04/2021</td>
-                    <td>Steve Rogers</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Clint Barton</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>Pam Beesley</td>
+                    <td>67</td>
+                    <td>26</td>
+                    <td>15021.00</td>
                 </tr>
             </tbody>
         </table>
