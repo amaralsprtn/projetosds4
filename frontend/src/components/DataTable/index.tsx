525c06d4196ce40cbf6482cd0da284f9621e0d54
Antonio Amaral <antonio.amaral@monitoratec.com.br>
2021-09-10 22:07:26 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,27 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
 const DataTable = () => {
+
+    const [page,setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+            .then(response => {
+                setPage(response.data);
+            });
+    }, [])
+    
     return (
         <div className="table-responsive">
         <table className="table table-striped table-sm">
@@ -12,48 +35,15 @@ const DataTable = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Dwight Schrute</td>
-                    <td>15</td>
-                    <td>22</td>
-                    <td>15016.00</td>
+                {page.content?.map(item => (
+                    <tr key={item.id}>
+                    <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                    <td>{item.seller.name}</td>
+                    <td>{item.visited}</td>
+                    <td>{item.deals}</td>
+                    <td>{item.amount.toFixed(2)}</td>
                 </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Phyllys Vance</td>
-                    <td>3</td>
-                    <td>23</td>
-                    <td>15017.00</td>
-                </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Stanley</td>
-                    <td>323</td>
-                    <td>24</td>
-                    <td>15018.00</td>
-                </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Jim Halpert</td>
-                    <td>1</td>
-                    <td>25</td>
-                    <td>15019.00</td>
-                </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Ryan</td>
-                    <td>57</td>
-                    <td>27</td>
-                    <td>15020.00</td>
-                </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Pam Beesley</td>
-                    <td>67</td>
-                    <td>26</td>
-                    <td>15021.00</td>
-                </tr>
+                ))}  
             </tbody>
         </table>
     </div>
