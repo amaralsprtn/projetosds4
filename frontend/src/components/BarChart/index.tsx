b0a9e1157ae68e33dce9252a4291544d6e36b8ab
Antonio Amaral <antonio.amaral@monitoratec.com.br>
2021-09-10 21:07:32 -0300

    First request

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,25 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
 
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 const BarChart = () => {
+
+    let chartData : ChartData = { labels: [], series: []};
+    
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then((response) => {
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x=> x.sum);
+
+            chartData = { labels: myLabels, series: mySeries};
+            console.log(response.data);
+        });
 
     const options = {
         plotOptions: {
@@ -10,22 +29,22 @@ const BarChart = () => {
         },
     };
     
-    const mockData = {
-        labels: {
-            categories: ['Pam', 'Jim', 'Stanley', 'Phyllys', 'Ryan', 'Dwight']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-            }
-        ]
-    };
+    // const mockData = {
+    //     labels: {
+    //         categories: ['Pam', 'Jim', 'Stanley', 'Phyllys', 'Ryan', 'Dwight']
+    //     },
+    //     series: [
+    //         {
+    //             name: "% Sucesso",
+    //             data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
+    //         }
+    //     ]
+    // };
 
     return (
         <Chart 
-            options ={{...options, xaxis: mockData.labels}}
-            series= {mockData.series}
+            options ={{...options, labels: chartData.labels}}
+            series= {chartData.series}
             type= "bar"
             height="240"
         />
