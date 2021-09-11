09ff51fa280f2616b3e839a37a23a783c7b1bd7b
Antonio Amaral <antonio.amaral@monitoratec.com.br>
2021-09-10 21:46:26 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,25 +1,56 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
-import { SaleSum } from 'types/sale';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
 import { BASE_URL } from 'utils/requests';
 
-type ChartData = {
-    labels: string[];
-    series: number[];
+type SeriesData = {
+    name: string;
+    data: number[];
 }
+
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+}
+
 const BarChart = () => {
 
-    let chartData : ChartData = { labels: [], series: []};
-    
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then((response) => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x=> x.sum);
+   const [chartData, setChartData] = useState<ChartData>({
+       labels: {
+           categories: []
+       },
+       series: [
+           {
+               name: "",
+               data: []
+           }
+       ]
+   });
 
-            chartData = { labels: myLabels, series: mySeries};
-            console.log(response.data);
+   useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`)
+    .then((response) => {
+      const data = response.data as SaleSuccess[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => round(100* x.deals / x.visited, 1));
+
+        setChartData({
+            labels: {
+                categories: myLabels
+            },
+            series: [
+                {
+                    name: "% Success",
+                    data: mySeries
+                }
+            ]
         });
+    });
+  }, []);
 
     const options = {
         plotOptions: {
@@ -29,21 +60,9 @@ const BarChart = () => {
         },
     };
     
-    // const mockData = {
-    //     labels: {
-    //         categories: ['Pam', 'Jim', 'Stanley', 'Phyllys', 'Ryan', 'Dwight']
-    //     },
-    //     series: [
-    //         {
-    //             name: "% Sucesso",
-    //             data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-    //         }
-    //     ]
-    // };
-
     return (
         <Chart 
-            options ={{...options, labels: chartData.labels}}
+            options ={{...options, xaxis: chartData.labels}}
             series= {chartData.series}
             type= "bar"
             height="240"
